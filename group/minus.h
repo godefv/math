84d36f953d14a69735a181c2ef6e08dcfcd208c4
godeffroy <gvalet@techviz.net>
2017-07-26 16:00:06 +0000

    group/minus - passage en metaprog par valeurs

--- group/minus.h
+++ group/minus.h
@@ -9,9 +9,14 @@ namespace group{
 	//! minus one commutes with every other element and squares to one under Operator 
 	//! minus_t<Operator, A> equals minus one times A under Operator
 	template<class Operator, class A> struct generated_minus_t{A value;};
-	template<class Operator, class A> struct minus_impl_t{using type=generated_minus_t<Operator, A>;};
-	template<class Operator, class A> struct minus_impl_t<Operator, generated_minus_t<Operator, A>>{using type=A;};
-	template<class Operator, class A> using minus_t=typename minus_impl_t<Operator, A>::type;
+
+	template<class Operator> constexpr auto minus(auto const& a){
+		return generated_minus_t<Operator, std::decay_t<decltype(a)>>{a};
+	}
+	template<class Operator> constexpr auto minus(generated_minus_t<Operator,auto> const& a){
+		return a.value;
+	}
+	template<class Operator, class A> using minus_t=decltype(minus<Operator>(A{}));
 
 	template<class Operator, class> struct is_generated_minus:std::false_type{};
 	template<class Operator, class A> struct is_generated_minus<Operator, generated_minus_t<Operator,A>>:std::true_type{};
