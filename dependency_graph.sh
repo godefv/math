cda00809f30d6eb9c3a7c2d35be930e2db397bb9
god <god@godfix>
2017-08-03 00:33:24 +0200

    dependency_graph.sh - support linux

--- dependency_graph.sh
+++ dependency_graph.sh
@@ -8,11 +8,13 @@ node_from_path(){
 
 echo "strict digraph dependency_graph {"
 echo "ratio=fill"
+echo "size=\"16,12\""
 
 for file in $files; do
 	node=$(node_from_path $file)
 	[[ $file =~ ".test.cpp" ]] && echo "${node} [shape=box]"
-	(for include_path in `/usr/local/bin/g++-7.1.0.exe $file -I/cygdrive/c/local/boost_1_64_0 -E -H 2>&1 |grep "^\. [^/]"|sed "s/^\. //"`; do node_from_path $include_path; done)|sed "s@.*@${node} -> \0@"
+	#(for include_path in `/usr/local/bin/g++-7.1.0.exe $file -I/cygdrive/c/local/boost_1_64_0 -E -H 2>&1 |grep "^\. [^/]"|sed "s/^\. //"`; do node_from_path $include_path; done)|sed "s@.*@${node} -> \0@"
+	(for include_path in `g++ $file -std=c++17 -fconcepts -E -H 2>&1 >/dev/null |grep "^\. [^/]"|sed "s/^\. //"`; do node_from_path $include_path; done)|sed "s@.*@${node} -> \0@"
 done
 
 groups=$((for file in $files; do node_from_path $(dirname $file); done)|sort|uniq)
