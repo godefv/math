c27262b99efc0188509e862ecc8570c53f4f1e06
godeffroy <gvalet@techviz.net>
2017-08-02 17:29:59 +0000

    ajout de dependency_graph.sh

---
+++ dependency_graph.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+files="$@"
+
+node_from_path(){
+	echo \"$(realpath --relative-to=. $1 | sed "s/\.[hc]$//;s/\.cpp$//")\"
+}
+
+echo "strict digraph dependency_graph {"
+echo "ratio=fill"
+
+for file in $files; do
+	node=$(node_from_path $file)
+	[[ $file =~ ".test.cpp" ]] && echo "${node} [shape=box]"
+	(for include_path in `/usr/local/bin/g++-7.1.0.exe $file -I/cygdrive/c/local/boost_1_64_0 -E -H 2>&1 |grep "^\. [^/]"|sed "s/^\. //"`; do node_from_path $include_path; done)|sed "s@.*@${node} -> \0@"
+done
+
+groups=$((for file in $files; do node_from_path $(dirname $file); done)|sort|uniq)
+for group in $groups; do
+	subgraph=$(echo $group | sed "s,^\"\(.*\)\"$,cluster_\1,;s,/,_,g;s,\.,root,")
+	echo "subgraph ${subgraph} {"
+	for file in $files; do
+		node=$(node_from_path $file)
+		[[ $(node_from_path $(dirname $file)) == $group ]] && echo "${node}"
+	done
+	echo "}"
+done
+
+echo "}"
+
