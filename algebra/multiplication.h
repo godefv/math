ec57ab1932e6161d6155be1949bec013b132e225
god <god@godfix>
2017-08-05 18:31:33 +0200

    algebra/multiplication - ajout de la multiplication par zero

--- algebra/multiplication.h
+++ algebra/multiplication.h
@@ -26,6 +26,13 @@ namespace algebra{
 		static constexpr auto apply(ElementT const& a, ScalarT const& b){
 			return apply(b,a);
 		}
+		//operation with zero
+		static constexpr auto apply(group::identity_t<vector::add_operation_t<auto>> zero_, basis_element_t<auto> const&){
+			return zero_;
+		}
+		static constexpr auto apply(basis_element_t<auto> const&, group::identity_t<vector::add_operation_t<auto>> zero_){
+			return zero_;
+		}
 		//operation with basis vectors
 		template<class ElementA, class ElementB> 
 		static constexpr auto apply(basis_element_t<ElementA> const& a, basis_element_t<ElementB> const& b){
