6b3f12e5fe84b984ee4dc359a2d88feaa144690a
god <god@godfix>
2017-10-13 21:56:21 +0200

    symbolic/trigonometry - màj du test (mieux lisible)

--- symbolic/trigonometry.test.cpp
+++ symbolic/trigonometry.test.cpp
@@ -3,15 +3,15 @@
 
 int main(){
 	using namespace symbolic;
-	check_equal(sin(angle_t<ratio_t<0,2>>{}), integer<0>);
-	check_equal(sin(angle_t<ratio_t<1,2>>{}), integer<1>);
-	check_equal(sin(angle_t<ratio_t<2,2>>{}), integer<0>);
-	check_equal(sin(angle_t<ratio_t<3,2>>{}), integer<-1>);
-	check_equal(sin(angle_t<ratio_t<4,2>>{}), integer<0>);
-	check_equal(sin(angle_t<ratio_t<5,2>>{}), integer<1>);
-	check_equal(sin(angle_t<ratio_t<6,2>>{}), integer<0>);
-	check_equal(sin(angle_t<ratio_t<7,2>>{}), integer<-1>);
-	check_equal(sin(angle_t<ratio_t<8,2>>{}), integer<0>);
+	check_equal(sin(ratio<0,2>*half_turn), integer<0>);
+	check_equal(sin(ratio<1,2>*half_turn), integer<1>);
+	check_equal(sin(ratio<2,2>*half_turn), integer<0>);
+	check_equal(sin(ratio<3,2>*half_turn), integer<-1>);
+	check_equal(sin(ratio<4,2>*half_turn), integer<0>);
+	check_equal(sin(ratio<5,2>*half_turn), integer<1>);
+	check_equal(sin(ratio<6,2>*half_turn), integer<0>);
+	check_equal(sin(ratio<7,2>*half_turn), integer<-1>);
+	check_equal(sin(ratio<8,2>*half_turn), integer<0>);
 	return 0;
 }
 
