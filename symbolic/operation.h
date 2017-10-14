c3791127778a17c72a5a05e98fec62e61936f4a7
god <god@godfix>
2017-10-15 00:23:43 +0200

    symbolic/operation.h - fusion de pow et nth_root, ajout de inverse et mult

--- symbolic/operation.h
+++ symbolic/operation.h
@@ -1,10 +1,12 @@
 #ifndef SYMBOLIC_OPERATION_H
 #define SYMBOLIC_OPERATION_H 
 
+#include"rational.h"
 #include"eval.h"
 #include<boost/hana.hpp>
 #include<iostream>
 #include<cmath>
+#include<type_traits>
 
 namespace symbolic{
 	template<class OperationT, class... OperandsT>
@@ -16,7 +18,16 @@ namespace symbolic{
 		constexpr operation_t(OperationT const& operation_in, OperandsT const&... operands_in)
 			:operation{operation_in}, operands{boost::hana::make_tuple(operands_in...)}
 		{}
+
+		auto constexpr operand() const{
+			return boost::hana::front(operands);
+		}
 	};
+
+	template<class> struct is_operation:std::false_type{};
+	template<class OperationT, class... OperandsT>
+	struct is_operation<operation_t<OperationT, OperandsT...>>:std::true_type{};
+	template<class T> concept bool Operation=is_operation<T>::value;
 
 	template<class OperationT, class... OperandsT>
 	std::ostream& operator<<(std::ostream& out, operation_t<OperationT, OperandsT...> const& operation){
@@ -25,11 +36,11 @@ namespace symbolic{
 		return out<<")";
 	}
 
-#define DEFINE_OPERATION(op, ...) \
+#define DEFINE_OPERATION(op) \
 	struct op##_t{}; \
 	inline std::ostream& operator<<(std::ostream& out, op##_t const){return out<<#op;} \
 	template<class... OperandsT> auto constexpr op(OperandsT const&... operands){return operation_t{op##_t{}, operands...};} \
-	auto constexpr eval(operation_t<op##_t, auto> const& operand){using std::op; return op(eval(boost::hana::front(operand.operands)));}
+	auto constexpr eval(operation_t<op##_t, auto> const& operand){using std::op; return op(eval(operand.operand()));}
 	DEFINE_OPERATION(exp)
 	DEFINE_OPERATION(log)
 	DEFINE_OPERATION(abs)
@@ -42,50 +53,79 @@ namespace symbolic{
 	DEFINE_OPERATION(cosh)
 	DEFINE_OPERATION(acosh)
 #undef DEFINE_OPERATION
+
+#define DEFINE_OPERATION(op, symbol) \
+	struct op##_t{}; \
+	inline std::ostream& operator<<(std::ostream& out, op##_t const){return out<<#op;} \
+	namespace operators{ \
+	auto constexpr operator symbol(auto const& a, auto const& b){return operation_t{op##_t{}, a,b};} \
+	} \
+	auto constexpr eval(operation_t<op##_t, auto> const& operand){using namespace boost::hana::literals; return eval(operand.operands[0_c]) symbol eval(operand.operands[1_c]);}
+	DEFINE_OPERATION(mult, *)
+#undef DEFINE_OPERATION
+
 	//pow
-	template<std::uintmax_t>
-	struct pow_t{};
+	template<Ratio RatioT>
+	struct pow_t{RatioT exponent;};
+	template<Ratio RatioT>
+	using nth_root_t=pow_t<decltype(inverse(RatioT{}))>;
 
-	template<std::uintmax_t N>
-	inline std::ostream& operator<<(std::ostream& out, pow_t<N> const){return out<<N<<"th_power";}
-	inline std::ostream& operator<<(std::ostream& out, pow_t<2> const){return out<<"square";}
-	inline std::ostream& operator<<(std::ostream& out, pow_t<3> const){return out<<"cube";}
+	inline std::ostream& operator<<(std::ostream& out, pow_t<auto> const power_operation){return out<<"power<"<<power_operation.exponent<<">";}
+	template<std::intmax_t N>
+	inline std::ostream& operator<<(std::ostream& out, pow_t<integer_t<N>> const){return out<<N<<"th_power";}
+	inline std::ostream& operator<<(std::ostream& out, pow_t<integer_t<2>> const){return out<<"square";}
+	inline std::ostream& operator<<(std::ostream& out, pow_t<integer_t<3>> const){return out<<"cube";}
+	template<std::intmax_t N>
+	inline std::ostream& operator<<(std::ostream& out, pow_t<ratio_t<1,N>> const){return out<<N<<"th_root";}
+	inline std::ostream& operator<<(std::ostream& out, nth_root_t<integer_t<2>> const){return out<<"sqrt";}
+	inline std::ostream& operator<<(std::ostream& out, nth_root_t<integer_t<3>> const){return out<<"cubic_root";}
 
-	template<std::uintmax_t N> std::string_view constexpr exponent_string;
 	using namespace std::string_view_literals;
-	template<> std::string_view constexpr exponent_string<2> ="²"sv;
-	template<> std::string_view constexpr exponent_string<3> ="³"sv;
-	template<> std::string_view constexpr exponent_string<4> ="⁴"sv;
-	template<std::uintmax_t N>
-	inline std::ostream& operator<<(std::ostream& out, operation_t<pow_t<N>, auto> const& operation) requires exponent_string<N>.data()!=nullptr {
-		return out<<"("<<boost::hana::front(operation.operands)<<")"<<exponent_string<N>;
+	std::string_view constexpr exponent_string(integer_t<2>){return "²"sv;}
+	std::string_view constexpr exponent_string(integer_t<3>){return "³"sv;}
+	std::string_view constexpr exponent_string(integer_t<4>){return "⁴"sv;}
+	template<Ratio RatioT> requires requires(){exponent_string(RatioT{});}
+	inline std::ostream& operator<<(std::ostream& out, operation_t<pow_t<RatioT>, auto> const& operation){
+		return out<<"("<<operation.operand()<<")"<<exponent_string(RatioT{});
+	}
+	template<Ratio RatioT> requires !requires(){exponent_string(RatioT{});} && requires(){exponent_string(inverse(RatioT{}));} 
+	inline std::ostream& operator<<(std::ostream& out, operation_t<pow_t<RatioT>, auto> const& operation){
+		return out<<exponent_string(inverse(RatioT{}))<<"√("<<operation.operand()<<")";
 	}
 	
-	template<std::uintmax_t N>
-	auto constexpr pow(auto const& operand){return operation_t{pow_t<N>{}, operand};}
-	auto constexpr square(auto const& operand){return operation_t{pow_t<2>{}, operand};}
-
-	//nth_root
-	template<std::uintmax_t>
-	struct nth_root_t{};
-
-	template<std::uintmax_t N>
-	inline std::ostream& operator<<(std::ostream& out, nth_root_t<N> const){return out<<N<<"th_root";}
-	inline std::ostream& operator<<(std::ostream& out, nth_root_t<2> const){return out<<"sqrt";}
-	inline std::ostream& operator<<(std::ostream& out, nth_root_t<3> const){return out<<"cubic_root";}
-	
-	template<std::uintmax_t N>
-	inline std::ostream& operator<<(std::ostream& out, operation_t<nth_root_t<N>, auto> const& operation) requires exponent_string<N>.data()!=nullptr {
-		return out<<exponent_string<N><<"√("<<boost::hana::front(operation.operands)<<")";
-	}
-
-	template<std::uintmax_t N>
-	auto constexpr nth_root(auto const& operand){return operation_t{nth_root_t<N>{}, operand};}
+	template<std::intmax_t N>
+	auto constexpr pow(auto const& operand){return operation_t{pow_t<integer_t<N>>{}, operand};}
+	template<Ratio RatioT>
+	auto constexpr pow(auto const& operand){return operation_t{pow_t<RatioT>{}, operand};}
+	template<std::intmax_t N>
+	auto constexpr nth_root(auto const& operand){return pow<ratio_t<1,N>>(operand);}
+	auto constexpr square(auto const& operand){return pow<2>(operand);}
 	auto constexpr sqrt(auto const& operand){return nth_root<2>(operand);}
-	auto constexpr eval(operation_t<nth_root_t<2>, auto> const& operand){
+
+	auto constexpr eval(operation_t<nth_root_t<integer_t<2>>, auto> const& operand){
 		using std::sqrt; 
 		using ::eval;
-		return sqrt(eval(boost::hana::front(operand.operands)));}
+		return sqrt(eval(operand.operand()));
+	}
+
+	//inverse
+	struct inverse_t{};
+	inline std::ostream& operator<<(std::ostream& out, inverse_t const){return out<<"inverse";} 
+	auto constexpr inverse(auto const& operand){return operation_t{inverse_t{}, operand};} 
+	
+	template<Ratio RatioT>
+	auto constexpr inverse(operation_t<pow_t<RatioT>,auto> const& operand){
+		return pow<RatioT>(inverse(operand.operand()));
+	} 
+	
+	auto constexpr eval(operation_t<inverse_t, auto> const& operand){
+		auto evaluated_operand=eval(operand.operand());
+		if constexpr(std::is_same<decltype(evaluated_operand), double>::value){
+			return 1./evaluated_operand;
+		}else{
+			return inverse(evaluated_operand);
+		}
+	}
 
 }
 
--- symbolic/operation.test.cpp
+++ symbolic/operation.test.cpp
@@ -5,15 +5,18 @@ int main(){
 	using namespace symbolic;
 	std::cout<<exp(integer<3>)<<std::endl;
 	std::cout<<sin(integer<3>)<<std::endl;
-	std::cout<<nth_root<2>(integer<4>)<<std::endl;
-	std::cout<<nth_root<3>(integer<4>)<<std::endl;
-	std::cout<<nth_root<4>(integer<4>)<<std::endl;
-	std::cout<<nth_root<5>(integer<4>)<<std::endl;
-	std::cout<<pow<2>(integer<4>)<<std::endl;
-	std::cout<<pow<3>(integer<4>)<<std::endl;
-	std::cout<<pow<4>(integer<4>)<<std::endl;
-	std::cout<<pow<5>(integer<4>)<<std::endl;
-	std::cout<<square(sin(sqrt(integer<4>)))<<std::endl;
+	std::cout<<nth_root<2>(integer<5>)<<std::endl;
+	std::cout<<nth_root<3>(integer<5>)<<std::endl;
+	std::cout<<nth_root<4>(integer<5>)<<std::endl;
+	std::cout<<nth_root<5>(integer<5>)<<std::endl;
+	std::cout<<pow<2>(integer<5>)<<std::endl;
+	std::cout<<pow<3>(integer<5>)<<std::endl;
+	std::cout<<pow<4>(integer<5>)<<std::endl;
+	std::cout<<pow<5>(integer<5>)<<std::endl;
+	std::cout<<pow<ratio_t<2,5>>(integer<5>)<<std::endl;
+	std::cout<<square(sin(sqrt(integer<5>)))<<std::endl;
+	using namespace symbolic::operators;
+	std::cout<<integer<5>*exp(integer<5>)<<std::endl;
 	return 0;
 }
 
