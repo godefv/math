d0c2aa0237c45419e722e6c2399426f53ff32b2a
god <god@godfix>
2018-03-11 17:29:34 +0100

    unit_test.h - support de hana::set             - check_equal n'échoue plus à la compilation mais seulement à l'exécution

--- unit_test.h
+++ unit_test.h
@@ -6,12 +6,29 @@
 
 namespace boost::hana{
 	template<class T, class... Ts>
-	std::ostream& operator<<(std::ostream& out, tuple<T, Ts...> const& foldable){
+	std::ostream& operator<<(std::ostream& out, set<T, Ts...> const& foldable){
+		out<<"<";
+		return fold(foldable, std::cout, [](std::ostream& o, auto const& value)->std::ostream&{return o<<value<<", ";})<<">";
+	}
+	template<template<class...> class List, class T, class... Ts> requires std::is_same<List<>,tuple<>>::value
+	std::ostream& operator<<(std::ostream& out, List<T, Ts...> const& foldable){
 		out<<"<"<<front(foldable);
 		return fold(drop_front(foldable), std::cout, [](std::ostream& o, auto const& value){return o<<value<<", ";})<<">";
 	}
 }
 
+void print_type(auto const& a){
+	std::cout<<typeid(a).name()<<std::endl;
+}
+
+void check_equal(auto const& a, auto const& b){
+	std::cerr<<"different types: "<<std::endl;
+	std::cerr<<"  "<<typeid(a).name()<<std::endl;
+	std::cerr<<"≠ "<<typeid(b).name()<<std::endl;
+	std::cerr<<"  "<<a<<std::endl;
+	std::cerr<<"≠ "<<b<<std::endl;
+	std::exit(1);
+}
 template<class T>
 void check_equal(T const& a, T const& b){
 	if(a==b){
@@ -30,10 +47,6 @@ void check_less(T const& a, T const& b){
 	}else{
 		std::cerr<<a<<" < "<<b<<std::endl;
 	}
-}
-
-void print_type(auto const& a){
-	std::cout<<typeid(a).name()<<std::endl;
 }
 
 inline auto unused(auto const& a){ static_cast<void>(a); }
