8002c208194b4f91e0b7f47a1ba9a17b65b96cf4
god <god@godfix>
2018-03-11 17:31:24 +0100

    geometry/rotation.test.cpp - vérifie plus de choses de manière automatique, moins de manière manuelle

--- geometry/rotation.test.cpp
+++ geometry/rotation.test.cpp
@@ -7,30 +7,73 @@ static constexpr auto e1=vector::unit_t<group::geometric::direction_positive_t<1
 static constexpr auto e2=vector::unit_t<group::geometric::direction_positive_t<2>>{};
 static constexpr auto e3=vector::unit_t<group::geometric::direction_negative_t<3>>{};
 
+auto test_bivector(geometry::simple_rotation_t<auto,auto,auto> const& rotation){
+	auto B=rotation.bivector();
+
+	std::cout<<"grade of bivector(): ";
+	check_equal(algebra::geometric::grades(B), algebra::geometric::grades<2>());
+
+	using namespace boost::hana::literals;
+	using namespace algebra::geometric::operators;
+
+	check_equal(B^rotation.plane.directions[0_c], algebra::geometric::zero);
+	check_equal(B^rotation.plane.directions[1_c], algebra::geometric::zero);
+	check_equal(algebra::geometric::grades(B|(rotation.plane.directions[1_c]^rotation.plane.directions[0_c])), algebra::geometric::grades<0>());
+	std::cout<<std::endl;
+}
+
+auto test_rotor(geometry::simple_rotation_t<auto,auto,auto> const& rotation){
+	auto B=rotation.bivector();
+	//auto R=rotation.rotor();
+
+	auto constexpr half_B=symbolic::ratio<1,2>*B;
+	using namespace algebra::geometric::operators;
+	auto constexpr square_half_B=half_B*half_B;
+
+	std::cout<<"cyclic blade "<<rotation.plane.blade()<<std::endl;
+	std::cout<<"angle "<<rotation.angle<<std::endl;
+	std::cout<<"bivector "<<B<<std::endl;
+	std::cout<<"half bivector "<<half_B<<std::endl;
+	std::cout<<"square "<<square_half_B<<std::endl;
+	std::cout<<"scalar "<<square_half_B.coordinate<<std::endl;
+	std::cout<<"scalar abs "<<abs(square_half_B.coordinate)<<std::endl;
+	std::cout<<"sqrt scalar abs "<<sqrt(abs(square_half_B.coordinate))<<std::endl;
+	std::cout<<"normalized half bivector "<<algebra::geometric::normalized(half_B)<<std::endl;
+	std::cout<<std::endl;
+}
+
+auto test_rotation(geometry::simple_rotation_t<auto,auto,auto> const& rotation){
+	using namespace boost::hana::literals;
+	using namespace algebra::geometric::operators;
+	static constexpr auto I=e0^e1^e2^e3;
+	auto plane=(rotation.plane.directions[1_c]^rotation.plane.directions[0_c]);
+	auto ortho=plane|I;
+	check_equal(rotation(plane), plane);
+	check_equal(rotation(ortho), ortho);
+}
+
 int main(){
 	using symbolic::half_turn;
 
 	auto constexpr rotation1=geometry::simple_rotation_t{geometry::plane(e1,e2), symbolic::ratio<1,2>*half_turn};
 	auto constexpr rotation2=geometry::simple_rotation_t{geometry::plane(e1,e3), symbolic::ratio<1,2>*half_turn};
-	//auto constexpr rotation3=geometry::simple_rotation_t{geometry::plane(e0,e3), symbolic::ratio<1,2>*half_turn};
+	auto constexpr rotation3=geometry::simple_rotation_t{geometry::plane(e0,e3), symbolic::ratio<1,2>*half_turn};
+
+	test_bivector(rotation1);
+	test_bivector(rotation2);
+	test_bivector(rotation3);
+
+	//test_rotor(rotation1);
+	//test_rotor(rotation2);
+	//test_rotor(rotation3);
+
+	test_rotation(rotation1);
+	test_rotation(rotation2);
+	test_rotation(rotation3);
+
 	auto constexpr R1=rotation1.rotor();
 	auto constexpr R2=rotation2.rotor();
-	auto constexpr B1=rotation1.bivector();
-	auto constexpr half_B1=symbolic::ratio<1,2>*B1;
 	using namespace algebra::geometric::operators;
-	auto constexpr square_half_B1=half_B1*half_B1;
-
-	using namespace algebra::geometric::operators;
-
-	std::cout<<"cyclic blade "<<rotation1.plane.blade()<<std::endl;
-	std::cout<<"angle "<<rotation1.angle<<std::endl;
-	std::cout<<"bivector "<<B1<<std::endl;
-	std::cout<<"half bivector "<<half_B1<<std::endl;
-	std::cout<<"square "<<square_half_B1<<std::endl;
-	std::cout<<"scalar "<<square_half_B1.coordinate<<std::endl;
-	std::cout<<"scalar abs "<<abs(square_half_B1.coordinate)<<std::endl;
-	std::cout<<"sqrt scalar abs "<<sqrt(abs(square_half_B1.coordinate))<<std::endl;
-	std::cout<<"normalized half bivector "<<algebra::geometric::normalized(half_B1)<<std::endl;
 
 	std::cout<<"cyclic quaternion("<<e1.element<<","<<e2.element<<"): "<<R1<<std::endl;
 	std::cout<<e1+e3     <<"->"; check_equal(rotation1(e1+e3), e3-e2); 
