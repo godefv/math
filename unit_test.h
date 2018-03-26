9ab9b21037d03ef9d88dc1a9a8177da7039a0096
god <god@godfix>
2018-03-26 15:57:41 +0200

    unit_test - check_equal compile à nouveau pour des types différents

--- unit_test.h
+++ unit_test.h
@@ -17,24 +17,23 @@ namespace boost::hana{
 	}
 }
 
+template<class T> struct is_hana_set:std::false_type{};
+template<class... Ts> struct is_hana_set<boost::hana::set<Ts...>>:std::true_type{};
+
 void print_type(auto const& a){
 	std::cout<<typeid(a).name()<<std::endl;
 }
 
 void check_equal(auto const& a, auto const& b){
-	if(a==b){
-		std::cerr<<a<<" == "<<b<<std::endl;
-	}else{
-		std::cerr<<"different types: "<<std::endl;
-		std::cerr<<"  "<<typeid(a).name()<<std::endl;
-		std::cerr<<"≠ "<<typeid(b).name()<<std::endl;
-		std::cerr<<"  "<<a<<std::endl;
-		std::cerr<<"≠ "<<b<<std::endl;
-		std::exit(1);
-	}
+	std::cerr<<"different types: "<<std::endl;
+	std::cerr<<"  "<<typeid(a).name()<<std::endl;
+	std::cerr<<"≠ "<<typeid(b).name()<<std::endl;
+	std::cerr<<"  "<<a<<std::endl;
+	std::cerr<<"≠ "<<b<<std::endl;
+	std::exit(1);
 }
-template<class T>
-void check_equal(T const& a, T const& b){
+template<class A, class B> requires std::is_same<A,B>::value || (is_hana_set<A>::value && is_hana_set<B>::value)
+void check_equal(A const& a, B const& b){
 	if(a==b){
 		std::cerr<<a<<" == "<<b<<std::endl;
 	}else{
