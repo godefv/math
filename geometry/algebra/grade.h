3d947c68b0fa830809c409b47dd3e55debe39de9
god <god@godfix>
2018-03-27 14:42:22 +0200

    geometry/algebra/grade - commentaires sur les morphismes

--- geometry/algebra/grade.h
+++ geometry/algebra/grade.h
@@ -9,6 +9,7 @@ namespace math::geometry{
 	//grade of blades
 	auto constexpr grade(Scalar const&){return 0;}
 	auto constexpr grade(Direction const&){return 1;}
+	//grade is a morphism from * to +
 	Blade{Blade2}
 	auto constexpr grade(group::generated_by_operation_t<mult_operation_t, Blade,Blade2> const& a){
 		return grade(a.first)+grade(a.second);
@@ -19,16 +20,20 @@ namespace math::geometry{
 
 	namespace hana=boost::hana;
 
+	//!Convenience function to make a set of grades.
 	template<int... Is>
 	auto constexpr grades(){return hana::make_set(hana::llong_c<Is>...);}
-	auto constexpr grades(zero_t const&){return grades<>();}
+	
+	//grades of multivectors
 	auto constexpr grades(Blade const& a){return grades<grade(a)>();}
+	//grades is a morphism from (auto,+) to (hana::set,hana::union)
+	auto constexpr grades(zero_t const&){return grades<>();}
 	MultiVector{MultiVector2}
 	auto constexpr grades(group::generated_by_operation_t<add_operation_t, MultiVector,MultiVector2> const& a){
 		return hana::union_(grades(a.first), grades(a.second));
 	}
 
-	auto constexpr project(zero_t, auto){return zero;}
+	//project multivector onto a set of grades
 	auto constexpr project(Blade const& blade, auto grades){
 		if constexpr(hana::find(grades, hana::llong_c<grade(blade)>)==hana::nothing){
 			return zero;
@@ -36,6 +41,8 @@ namespace math::geometry{
 			return blade;
 		}
 	}
+	//project(x, auto) is an endomorphism over +
+	auto constexpr project(zero_t, auto){return zero;}
 	MultiVector{MultiVector2}
 	auto constexpr project(group::generated_by_operation_t<add_operation_t, MultiVector,MultiVector2> const& a, auto grades){
 		return project(a.first, grades)+project(a.second, grades);
