ad73fadb029e29a154d7a92548dace48755048bd
god <god@godfix>
2018-03-27 14:43:09 +0200

    symbolic/unit_test - ajout de symboles scalaires

--- symbolic/unit_test.h
+++ symbolic/unit_test.h
@@ -3,13 +3,26 @@
 
 #include"symbol.h"
 #include"../unit_test.h"
+#include"../scalar.h"
 #include<iostream>
 
 using x_t=math::symbol_t<math::symbol_name_t<'x'>>; 
 using y_t=math::symbol_t<math::symbol_name_t<'y'>>; 
 using z_t=math::symbol_t<math::symbol_name_t<'z'>>; 
+using k_t=math::symbol_t<math::symbol_name_t<'k'>>; 
+using l_t=math::symbol_t<math::symbol_name_t<'l'>>; 
+using m_t=math::symbol_t<math::symbol_name_t<'m'>>; 
 x_t x;
 y_t y;
 z_t z;
+k_t k;
+l_t l;
+m_t m;
+
+namespace math{
+	template<> struct is_scalar<k_t>:std::true_type {};
+	template<> struct is_scalar<l_t>:std::true_type {};
+	template<> struct is_scalar<m_t>:std::true_type {};
+}
 
 #endif /* SYMBOLIC_UNIT_TEST_H */
