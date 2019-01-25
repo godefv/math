784fc465275102d31fc9079267805a9c01d7b8fb
god <god@godfix>
2019-01-25 21:24:41 +0100

    dependency_graph.sh - nouveau graph.svg,graph_modules.svg - màj

--- dependency_graph.sh
+++ dependency_graph.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
 module_graph=0
-if [[ $1 == "--modules" ]]; then
-	module_graph=1
-	shift
-fi
+gcc_options=""
+
+while true; do
+    case $1 in
+        -h|-\?|--help)
+            echo "-m,--modules : treat each root files or directory as a single node"
+			echo "-i,--include : additionnal include directory"
+            exit
+            ;;
+        -m|--modules)
+			module_graph=1
+            ;;
+        -i|--include)
+			gcc_options+=" -I$2"
+			shift
+            ;;
+        --)              # End of all options.
+            shift
+            break
+			;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)               # Default case: No more options, so break out of the loop.
+            break
+    esac
+
+    shift
+done
 
 files="$@"
 
@@ -12,7 +37,7 @@ node_from_path(){
 	if [[ $module_graph == 0 ]]; then
 		echo \"$(realpath --relative-to=. $1 | sed "s/\.[hc]$//;s/\.cpp$//")\"
 	else
-		echo \"$(realpath --relative-to=. $1 | sed "s,/.*,,")\"
+		echo \"$(realpath --relative-to=. $1 | sed "s/\.[hc]$//;s/\.cpp$//;s,/.*,,")\"
 	fi
 }
 
@@ -20,8 +45,8 @@ echo "strict digraph dependency_graph {"
 
 for file in $files; do
 	node=$(node_from_path $file)
-	[[ $file =~ ".test.cpp" && $module_graph == 0 ]] && echo "${node} [shape=box]"
-	(for include_path in `g++ $file -std=c++17 -fconcepts -I. -E -H 2>&1 >/dev/null |grep "^\. [^/]"|sed "s/^\. //"`; do dependancy=$(node_from_path $include_path); [[ $dependancy != $node ]] && echo $dependancy; done)|sed "s@.*@${node} -> \0@"
+	[[ $node =~ ".test" ]] && echo "${node} [shape=box]"
+	(for include_path in `g++ $file -std=c++17 -fconcepts -I. -E -H $gcc_options 2>&1 >/dev/null |grep "^\. [^/]"|sed "s/^\. //"`; do dependancy=$(node_from_path $include_path); [[ $dependancy != $node ]] && echo $dependancy; done)|sed "s@.*@${node} -> \0@"
 done
 
 if [[ $module_graph == 0 ]]; then
