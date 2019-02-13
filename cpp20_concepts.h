489e4e1bfd0fe79dccb3457dbd69fae4b4417736
god <god@godfix>
2019-02-13 15:39:21 +0100

    geometry/object/frame - nouveau, permet de faire des changements de référentiel

---
+++ cpp20_concepts.h
@@ -0,0 +1,13 @@
+#ifndef CPP20_CONCEPTS_H
+#define CPP20_CONCEPTS_H 
+
+#include<functional>
+
+template< class F, class... Args >
+concept bool Invocable =
+requires(F f, Args&&... args) {
+	std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+	/* not required to be equality preserving */
+};
+
+#endif /* CPP20_CONCEPTS_H */
---
+++ hana.h
@@ -0,0 +1,40 @@
+#ifndef HANA_PIPE_H
+#define HANA_PIPE_H 
+
+#include<boost/hana.hpp>
+#include"cpp20_concepts.h"
+#include<type_traits>
+
+namespace boost::hana{
+	//default pipe operator
+	template<class T>
+	inline auto constexpr operator|(T const& input, Invocable<T> function){
+		return function(input);
+	}
+
+	//overload to take precedence over the operator defined for monads
+	template <typename Xs, typename = typename std::enable_if<
+		detail::monad_operators<typename hana::tag_of<Xs>::type>::value
+	>::type>
+	constexpr auto operator|(Xs&& input, Invocable<Xs>&& function){
+		return function(input);
+	}
+
+	template<class F>
+	struct transform_with_t{
+		F f;
+		auto constexpr operator()(auto const& operand) const{
+			return transform(operand,f);
+		}
+	};
+
+	template<class F>
+	inline auto constexpr transform_with(F&& f){
+		return transform_with_t<F>{std::forward<F>(f)};
+	}
+	inline auto constexpr fold_with(auto const& f){
+		return [&](auto const& x){return fold(x,f);};
+	}
+}
+
+#endif /* HANA_PIPE_H */
