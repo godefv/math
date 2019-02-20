9248b31f0951d6aa35fbde47dcbf88b92db1b198
god <god@godfix>
2019-02-20 08:39:09 +0100

    geometry/algebra/dot_product.h - restricted constraints

--- geometry/algebra/dot_product.h
+++ geometry/algebra/dot_product.h
@@ -15,15 +15,18 @@ namespace math::geometry{
 	};
 	using bilinear_dot_operation_t=bilinear_operation_t<dot_operation_t>;
 
-	//by default, use group operations
-	auto constexpr operator|(auto const& a, auto const& b){
-		return bilinear_operation_t<dot_operation_t>::apply(a,b);
-	}
-
+	//operation on blades
 	Blade{Blade2}
 	auto constexpr operator|(Blade const& a, Blade2 const& b){
 		return project(a*b, grades<abs(grade(a)-grade(b))>());
 	}
+	//extand operation by bilinearity
+	template<MultiVector A, MultiVector B> 
+		requires !(Blade<A> && Blade<B>)
+	auto constexpr operator|(A const& a, B const& b){
+		return bilinear_operation_t<dot_operation_t>::apply(a,b);
+	}
+
 }
 
 #endif /* GEOMETRY_ALGEBRA_DOT_PRODUCT_H */
