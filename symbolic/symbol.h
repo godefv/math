573607170ad1be2d89eb9e6a810e855e1c689d8d
god <god@godfix>
2019-02-20 13:41:15 +0100

    symbolic/symbol.h - un nom est un symbole

--- symbolic/symbol.h
+++ symbolic/symbol.h
@@ -6,26 +6,29 @@
 
 namespace math{
 	//!A Symbol is any type which represents a specific instance of a mathematical object. 
-	//!An abstract type x_t is a Symbol even if it can be assigned a value at runtime if it will represent only one specific value at a given time. 
+	//!An abstract type x_t is a Symbol even if it can be assigned a value at runtime if it will represent only one specific value
 	//!The int type is not a Symbol because it can represent any integer at runtime.
 	//!Some operations on even the most abstract symbols can be processed at compile time, such as x*x=x².
+
+	//Symbol concept, is_symbol<> can be specialized to make a type a Symbol
+	template<class T> struct is_symbol: std::false_type{};
+	template<class T> concept bool Symbol=is_symbol<T>::value;
+
+	//name_t is a Symbol
+	template<char... Name> struct is_symbol<name_t<Name...>>: std::true_type{};
 
 	//!Template to make Symbol types.
 	template<class Name>
 	struct symbol_t{
 		Name name;
 	};
+	template<Symbol Name> struct is_symbol<symbol_t<Name>>: std::true_type{};
 
 	//operators
 	template<class NameT>
 	bool operator==(symbol_t<NameT>,symbol_t<NameT>){return true;}
 	bool operator==(symbol_t<auto> a,symbol_t<auto> b){return false;}
 	bool operator!=(symbol_t<auto> a,symbol_t<auto> b){return !(a==b);}
-
-	//Symbol concept, is_symbol<> can be specialized to make a type a Symbol
-	template<class T> struct is_symbol: std::false_type{};
-	template<class Name> struct is_symbol<symbol_t<Name>>: std::true_type{};
-	template<class T> concept bool Symbol=is_symbol<T>::value;
 
 	template<char... letters>
 	auto constexpr symbol=symbol_t<name_t<letters...>>{};
