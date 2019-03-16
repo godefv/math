1331b91c0712607a640d6ac3be2429f2f996f5af
god <god@godpc>
2019-03-16 11:23:20 +0100

    geometry/vector_transform/perspective_projection - on projète sur un hyperplan et non un plan

--- geometry/vector_transform/perspective_projection.h
+++ geometry/vector_transform/perspective_projection.h
@@ -15,8 +15,9 @@ namespace math::geometry{
 
 	//apply
 	auto constexpr apply(PerspectiveProjection const& projection, Vector const& operand){
-		auto projected_orthographic=projection.orthographic(operand);
-		auto depth=norm(operand-projected_orthographic);
+		auto depth=operand|inverse(projection.direction);
+		//auto projected_orthographic=operand-depth*projection.direction;
+		auto projected_orthographic=project((operand^inverse(projection.direction))*projection.direction, grades(operand));
 		if constexpr(Symbol<decltype(depth)> && Symbol<decltype(projection.min_clip_distance)>){
 			if constexpr(eval(depth)<eval(decltype(projection.min_clip_distance){})){
 				return projected_orthographic*infinity;
@@ -27,6 +28,7 @@ namespace math::geometry{
 			using ::eval;
 			auto depth_runtime=eval(depth);
 			if(depth_runtime<eval(projection.min_clip_distance)){
+				using math::inverse;
 				return projected_orthographic*std::numeric_limits<decltype(inverse(depth_runtime))>::max();
 			}else{
 				return projected_orthographic/depth_runtime;
@@ -35,29 +37,27 @@ namespace math::geometry{
 	}
 
 	//projection
-	template<class SliceT, class DepthT=double>
+	template<Vector Direction, class ClipDistanceT>
 	struct perspective_projection_t{
-		projection_t<SliceT> orthographic;
-		DepthT min_clip_distance;
-		constexpr perspective_projection_t(SliceT const& s, DepthT const& d)
-			:orthographic{projection_t{s}}, min_clip_distance{d} {}
+		Direction direction;
+		ClipDistanceT min_clip_distance;
 		auto constexpr operator()(auto const& a) const{
 			return apply(*this, a);
 		}
 	};
-	template<class SliceT, class DepthT>
-	perspective_projection_t(SliceT const&i, DepthT const&)->perspective_projection_t<SliceT,DepthT>;
+	template<Vector Direction, class ClipDistanceT>
+	perspective_projection_t(Direction const&, ClipDistanceT const&)->perspective_projection_t<Direction, ClipDistanceT>;
 
 	template<class SliceT, class DepthT> struct is_perspective_projection<perspective_projection_t<SliceT,DepthT>>:std::true_type{};
 
-	std::ostream& operator<<(std::ostream& out, perspective_projection_t<auto> const& p){
+	std::ostream& operator<<(std::ostream& out, perspective_projection_t<auto,auto> const& p){
 		return out<<"perspective{"<<p.slice<<"}";
 	}
 
-	bool constexpr operator==(perspective_projection_t<auto> const& a, perspective_projection_t<auto> const& b){
+	bool constexpr operator==(perspective_projection_t<auto,auto> const& a, perspective_projection_t<auto,auto> const& b){
 		return a.slice==b.slice;
 	}
-	bool constexpr operator!=(perspective_projection_t<auto> const& a, perspective_projection_t<auto> const& b){
+	bool constexpr operator!=(perspective_projection_t<auto,auto> const& a, perspective_projection_t<auto,auto> const& b){
 		return !(a==b);
 	}
 }
--- geometry/vector_transform/perspective_projection.test.cpp
+++ geometry/vector_transform/perspective_projection.test.cpp
@@ -4,22 +4,21 @@
 int main(){
 	using math::integer;
 	using math::ratio;
-	auto constexpr projection1=math::geometry::perspective_projection_t{math::geometry::slice_t{e0,e1}, ratio<1,10>};
-	auto constexpr projection2=math::geometry::perspective_projection_t{math::geometry::slice_t{integer<2>*e0+integer<2>*e1,e2}, ratio<1,10>};
-	auto constexpr projection3=math::geometry::perspective_projection_t{math::geometry::slice_t{2.0*e0+2.0*e1,8.0*e2}, 0.1};
+	auto constexpr projection1=math::geometry::perspective_projection_t{e2, ratio<1,10>};
+	auto constexpr projection2=math::geometry::perspective_projection_t{normalized(e0-e1), ratio<1,10>};
+	auto constexpr projection3=math::geometry::perspective_projection_t{normalized(2.0*e0-2.0*e1), 0.1};
 
 	check_equal(projection1(e0), math::infinity*e0);
 	check_equal(projection1(e1), math::infinity*e1);
 	check_equal(projection1(e2), math::zero);
 	check_equal(projection1(2.0*e0+4.0*e2), 0.5*e0);
-	check_equal(projection1(2.0*e0-4.0*e2), 0.5*e0); //no clue about direction yet, so no front, no back
+	check_equal(projection1(2.0*e0-4.0*e2), std::numeric_limits<double>::infinity()*e0); 
 	check_equal(projection2(e0), ratio<1,2>*sqrt(integer<2>)*(e0+e1));
-	check_equal(projection2(e1), ratio<1,2>*sqrt(integer<2>)*(e0+e1));
+	check_equal(projection2(e1), ratio<1,2>*math::infinity*(e0+e1));
 	check_equal(projection2(e2), math::infinity*e2);
 	//check_equal(projection3(e0), );
 	//check_equal(projection3(e1), );
 	check_equal(projection3(e2), std::numeric_limits<double>::max()*e2);
-	check_equal(projection3(n1), math::zero);
 
 	return 0;
 }
