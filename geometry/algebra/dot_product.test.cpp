dd0a76e31bdef181e9822e76802a51522efe56b7
god <godeffroy.valet@m4x.org>
2020-03-31 14:27:54 +0200

    geometry/algebra/dot_product.test.cpp - test avec float

--- geometry/algebra/dot_product.test.cpp
+++ geometry/algebra/dot_product.test.cpp
@@ -10,6 +10,7 @@ int main(){
 	check_equal(e1|n1, zero);
 	check_equal(e1|(e2+e1), one);
 	check_equal((e1*e2)|(e1*e2), -one);
+	check_equal((1.0f*e1+2.0f*e2+3.0f*e3)|(0.0f*e1+0.0f*e2+1.0f*e3), 3.0f);
 	return 0;
 }
 
